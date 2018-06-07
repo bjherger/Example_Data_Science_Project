53abb2780a05ef10226431bf10cfb05470535f53
Brendan Herger <brendanherger@hergergolf.local>
2018-06-07 13:02:06 -0700

    First commit, adding starter repo items

---
+++ README.md
@@ -0,0 +1,31 @@
+# Python Starter Repo
+
+A brief, snappy description of what this project does, and why the audience cares
+
+## Quick start
+
+```python
+# Set up Anaconda virtual environment
+conda env create -f environment.yml --force
+
+# Activate Anaconda virtual environment
+source activate (environment_name)
+
+# Run code
+cd bin/
+python main.py
+```
+
+## Repo structure
+
+ - `bin/main.py`: Code entry point
+ - `conf/conf.py`: Configuration file for project
+
+### Python Environment
+Python code in this repo utilizes packages that are not part of the common library. To make sure you have all of the 
+appropriate packages, please install [Anaconda](https://www.continuum.io/downloads), and install the environment 
+described in environment.yml (Instructions [here](http://conda.pydata.org/docs/using/envs.html), under *Use 
+environment from file*, and *Change environments (activate/deactivate)*). 
+
+## Contact
+Feel free to contact me at 13herger `<at>` gmail `<dot>` com
---
+++ bin/main.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+"""
+coding=utf-8
+
+Code Template
+
+"""
+import cPickle
+import logging
+import os
+
+import pandas
+
+import lib
+
+
+def main():
+    """
+    Main function documentation template
+    :return: None
+    :rtype: None
+    """
+    logging.basicConfig(level=logging.DEBUG)
+
+    observations = extract()
+    observations = transform(observations)
+    observations, transformation_pipeline, trained_model = model(observations)
+    load(observations, transformation_pipeline, trained_model)
+    pass
+
+
+def extract():
+    logging.info('Begin extract')
+    observations = pandas.DataFrame()
+
+    lib.archive_dataset_schemas('extract', locals(), globals())
+    logging.info('End extract')
+    return observations
+
+
+def transform(observations):
+    logging.info('Begin transform')
+
+    lib.archive_dataset_schemas('transform', locals(), globals())
+    logging.info('End transform')
+    return observations
+
+
+def model(observations):
+    logging.info('Begin model')
+
+    mapper = None
+
+    transformation_pipeline = None
+
+    trained_model = None
+
+    lib.archive_dataset_schemas('model', locals(), globals())
+    logging.info('End model')
+    return observations, transformation_pipeline, trained_model
+
+
+def load(observations, transformation_pipeline, trained_model):
+    logging.info('Begin load')
+
+    # Reference variables
+    lib.get_temp_dir()
+
+    observations_path = os.path.join(lib.get_temp_dir(), 'observations.csv')
+    logging.info('Saving observations to path: {}'.format(observations_path))
+    observations.to_csv(observations_path, index=False)
+
+    if transformation_pipeline is not None:
+        transformation_pipeline_path = os.path.join(lib.get_temp_dir(), 'transformation_pipeline.pkl')
+        logging.info('Saving transformation_pipeline to path: {}'.format(transformation_pipeline))
+        cPickle.dump(transformation_pipeline, open(transformation_pipeline, 'w+'))
+
+    if trained_model is not None:
+        trained_model_path = os.path.join(lib.get_temp_dir(), 'trained_model.pkl')
+        logging.info('Saving trained_model to path: {}'.format(transformation_pipeline))
+        cPickle.dump(trained_model, open(trained_model_path, 'w+'))
+
+    lib.archive_dataset_schemas('load', locals(), globals())
+    logging.info('End load')
+    pass
+
+
+# Main section
+if __name__ == '__main__':
+    main()
